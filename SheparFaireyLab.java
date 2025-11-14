06077d4ffca7c39a6649f9fd731eb53f20534969
Joshua Lee <jylee2@stu.naperville203.org>
2025-11-14 11:27:44 -0600

    Fairy lab, change the colors for better difference compared to original

--- SheparFaireyLab.java
+++ SheparFaireyLab.java
@@ -40,15 +40,42 @@ public class SheparFaireyLab
           * method 1 change
           */
          int red, blue, green;
-         int RED, Dblue, Lblue, Owhite; // balanced approach color buckets
+         int RED, Db,Lb,Lg,Lr, Wr, Wg, Wb; // balanced approach color buckets
          for(Pixel Change1: Mpixels1 )
          {
             red = Change1.getRed();
             blue = Change1.getBlue();
             green = Change1.getGreen();
-            Dblue = (int)(blue * 3);
-            Lblue = (int)(blue * 0.5);
+            Lb = (int)(blue * 0.9);
+            Lg = (int)(green * 0.85);
+            Lr = (int)(red * 0.68);
+            Db = (int)(blue * 0.545);
+            RED = (int)(red * 1);
+            Wr = (int)(red * 0.961);
+            Wg = (int)(green * 0.95);
+            Wb = (int)(blue * 0.963);
+            Color Light = new Color(Lr,Lg,Lb);
+            Color Dark = new Color(0,0,Db);
+            Color PureR = new Color(RED,0,0);
+            Color Owhite = new Color (Wr,Wg,Wb);
+            if (red > 20 && red < 120 && green > 70 && green < 150 && blue > 130 && blue < 240)
+            {
+                Change1.setColor(PureR); // Make blue shirt RED
+            }
+            if (red > 25 && red < 145 && green > 5 && green < 140 && blue > 5 && blue < 120) // Make hair and background off white
+            {
+                Change1.setColor(Light);
+            }
+            if (red > 110 && red < 170 && green > 120 && green < 170 && blue > 120 && blue < 170)// Change lighter background around head to dark blue
+            {
+                Change1.setColor(Dark);
+            }
+            if(red > 145 && red < 250 && green > 120 && green < 240 && blue > 70 && blue < 200) // chang eface to light blue
+            {
+                Change1.setColor(Owhite);
+            }
          }
+         me.explore();
          /**
           * method 2 change
           * 
