1aa3b4ba296c5555d1074c3b0eb38a2163282c2d
Joshua Lee <jylee2@stu.naperville203.org>
2025-11-20 18:17:35 -0600

    Update SheparFaireyLab.java

--- SheparFaireyLab.java
+++ SheparFaireyLab.java
@@ -46,16 +46,16 @@ public class SheparFaireyLab
          int b, s, interval;
          int red, blue, green, gray;
          int RED, Db,Lb,Lg,Lr, Wr, Wg, Wb; // balanced approach color buckets
-         for(Pixel Change1: Mpixels1 )
+         for(Pixel ChangeG1: Mpixels1 )
          {
-            red = Change1.getRed();
-            blue = Change1.getBlue();
-            green = Change1.getGreen();
+            red = ChangeG1.getRed();
+            blue = ChangeG1.getBlue();
+            green = ChangeG1.getGreen();
            
             
             gray = (int)(red + blue + green) / 3;
             Color graycolor = new Color (gray, gray, gray);
-            Change1.setColor(graycolor);
+            ChangeG1.setColor(graycolor);
             // gray scale the entire photo and then sort rgb value values in intervals of 64 (1-4) for what bucket gets which part of the photo
             
             
@@ -66,14 +66,28 @@ public class SheparFaireyLab
             Color Owhite = new Color (Wr,Wg,Wb);
             */
          }
+         for (Pixel Change1 : Mpixels1)
+          {
+           // gets gray scale values
+           red = Change1.getColor(graycolor);
+           green = Change1.getColor(graycolor);
+           blue = Change1.getColor(graycolor);
+           // rgb values of color types,using pixel value from gray scale photo
+           Lr = (int)(red *);
+           Lb = (int)(blue *);
+           Lg = (int)(green *);
+           Dr = (int)(red *);
+           Dg = (int)(green *);
+           Db = (int)(blue *);
+           RED = (int)(red * 0.85)
+           Wr = (int)(red *);
+           Wb = (int)(blue *);
+           Wg = (int)(green *);
+           // set color below
+
+           // check values with intervals of 64 into groups of 4 (0-64, 65-128, etc)
+          }
          // set for loop to create shepard fairy, must pull from grayscale, not original photo
-         
-         
-         
-         
-         
-         
-         
          
          
         me1.explore();
@@ -81,110 +95,57 @@ public class SheparFaireyLab
           * method 2 change
           * 
           */
-         for (Pixel Change2: Mpixels2)
+         for (Pixel ChangeG2: Mpixels2)
           {
-           
            red = Change2.getRed();
            blue = Change2.getBlue();
            green = Change2.getGreen();
-           b = 0; // largest value in gray scale
-           s = 255; // smallest value in gray scale
-           
            gray = (red + blue + green) / 3;
-           if (gray < s)
-               s = gray;
-           if (gray > b)
-               b = gray;
-           
-           
            Color colorgray = new Color(gray,gray,gray);
            Change2.setColor(colorgray);
-           interval = (b - s) / 4; // interval divided evenly by choice in color
-           Lb = (int)(blue * 0.9);
-           Lg = (int)(green * 0.85);
-           Lr = (int)(red * 0.68);
-           Db = (int)(blue * 0.545);
-           RED = (int)(red * 1);
-           Wr = (int)(red * 0.971);
-           Wg = (int)(green * 0.96);
-           Wb = (int)(blue * 0.973);
+           
+           /*
            Color Light = new Color(Lr,Lg,Lb);
            Color Dark = new Color(0,0,Db);
            Color PureR = new Color(RED,0,0);
            Color Owhite = new Color (Wr,Wg,Wb);
-           if (red >= s && red < s + interval && green >= s && green < s + interval && blue >= s && blue < s + interval) // off white filling
-           {  
-            Change2.setColor(Owhite);
-           }
-           if (red >= (s + interval) && red < s + (interval*2) && green >= (s + interval) && green < s + (interval * 2) && blue >= (s + interval) && blue < 2 + (interval * 2)) // Pure Red filling
-           {
-            Change2.setColor(PureR);
-           }
-           if (red >= s + (2 * interval) && red < s + (3 * interval) && green >=  s + (2 * interval) && green < s + (3 * interval) && blue >=  s + (2 * interval) && blue < s + (3 * interval)) // Light Blue filling
-           {
-            Change2.setColor(Light);
-           }
-           if (red >= s + (interval * 3) && red <= b && green >= s + (interval * 3) && green <= b && blue >= s + (interval * 3) && blue <= b) // Dark Blue filling
-           {
-            Change2.setColor(Dark);
-           }
+           */  
         }
+        for (Pixel Change2: Mpixels2)
+         {
+           red = Change2.getColor(colorgray);
+           blue = Change2.getColor(colorgray);
+           green = Change2.getColor(colorgray);
+          // color values have already been set
+
+          // set b and s values for equation for interval
+          // interval is used to get range of largest rgb values and around equal filling
+         }
            me2.explore();
            // light green
            int Or, Ob, Og; // orange
            int Pr, Pb, Pg; // Pastel
            int Yr, Yb, Yg; // Intense Yellow
-           for (Pixel Change3: Mpixels3)
-            {
-             red = Change3.getRed();
-             blue = Change3.getBlue();
-             green = Change3.getGreen();
-             gray = (red + blue + green) / 3;
-             Color colorgray = new Color(gray,gray,gray);
-             b = 0;
-             s = 255;
-             if (gray < s)
-               s = gray;
-             if (gray > b)
-               b = gray;
-             Or = (int)(red * 1);
-             Og = (int)(green * 0.647);
-             Ob = (int)(blue * 0);
-             Pr = (int)(red * 1);
-             Pg = (int)(green * 0.41);
-             Pb = (int)(blue * 0.38);
-             Yr = (int)(red * 1);
-             Yg = (int)(blue * 0.937);
-             Yb = (int)(green * 0.161);
-             Lb = (int)(blue * 0.9);
-             Lg = (int)(green * 0.85);
-             Lr = (int)(red * 0.68);
-             Color Light = new Color(Lr,Lg,Lb);
-             Color Pastel = new Color(Pr,Pg,Pb);
-             Color Iyellow = new Color(Yr,Yg,Yb);
-             Color Orange = new Color (Lr,Lg,Lb);
-             Change3.setColor(colorgray);
-             // get colors for intense orange, Light green, Pastel, And intense yellow
-             // using method 2 as the perfered method of coloring
-             interval = (b - s) / 4; // interval divided evenly by choice in color
-             if (red >= s && red < s + interval && green >= s && green < s + interval && blue >= s && blue < s + interval)
-             {
-              Change3.setColor(Light);
-             }
-             if (red >= (s + interval) && red < s + (interval*2) && green >= (s + interval) && green < s + (interval * 2) && blue >= (s + interval) && blue < 2 + (interval * 2))
-             {
-              Change3.setColor(Pastel);
-             }
-             if (red >= s + (2 * interval) && red < s + (3 * interval) && green >=  s + (2 * interval) && green < s + (3 * interval) && blue >=  s + (2 * interval) && blue < s + (3 * interval))
-             {
-              Change3.setColor(Iyellow);
-             }
-             if (red >= s + (interval * 3) && red <= b && green >= s + (interval * 3) && green <= b && blue >= s + (interval * 3) && blue <= b)
-             {
-              Change3.setColor(Orange);
-             }
-            }
-        me3.explore();
+         for (Pixel ChangeG3: Mpixels3)
+          {
+           red = ChangeG3.getRed();
+           blue = ChangeG3.getBlue();
+           green = ChangeG3.getGreen();
+           gray = (red + blue + green) / 3;
+           Color colorgray = new Color(gray,gray,gray);
+           ChangeG3.setColor(colorgray);
+          }
+          for(Pixel Change3 : Mpixels3)
+           {
+            red = Change2.getColor(colorgray);
+            blue = Change2.getColor(colorgray);
+            green = Change2.getColor(colorgray);
+            // color values have already been set
+
+            // set b and s values for equation for interval
+            // interval is used to get range of largest rgb values and around equal filling
+           }
+          me3.explore();
         
            
            
