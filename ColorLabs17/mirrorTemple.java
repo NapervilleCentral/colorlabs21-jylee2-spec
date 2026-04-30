8e48337dfba4b2674aeb9f6cd57859555f7f8370
Joshua Lee <jylee2@stu.naperville203.org>
2026-04-30 08:29:08 -0500

    refer to modules for temple instrucitons

---
+++ ColorLabs17/mirrorTemple.java
@@ -0,0 +1,22 @@
+
+/**
+ * take image called temple and reflect the left side of the image so that the temple 
+ * has a complete sides
+ *
+ * @author (Joshua)
+ * @version (4/30/26)
+ */
+public class mirrorTemple
+{
+    // instance variables - replace the example below with your own
+  
+
+    /**
+     * Constructor for objects of class Mirror_Temple
+     */
+    public void mirrorTemple()
+    {
+        // initialise instance variables
+       
+    }
+}
