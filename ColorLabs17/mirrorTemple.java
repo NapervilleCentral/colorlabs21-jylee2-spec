1765d1b2267a300ed7d45c8136ed21bfac10915d
Joshua Lee <jylee2@stu.naperville203.org>
2026-05-01 08:10:33 -0500

    make the temple whole again.

--- ColorLabs17/mirrorTemple.java
+++ ColorLabs17/mirrorTemple.java
@@ -6,17 +6,31 @@
  * @author (Joshua)
  * @version (4/30/26)
  */
+import java.awt.*; // brings color class
+import java.util.*;
+import java.util.List;
 public class mirrorTemple
 {
-    // instance variables - replace the example below with your own
-  
-
+    public static void main(String [] args)
+    {
+        Picture Temple = new Picture("images/temple.jpg");
+        Pixel [] pixels;
+        Pixel [] Tpixels;
+        
+        Tpixels = Temple.getPixels();
+        
+        Temple.explore();
+        // Coordinates (0 , 0) starts at the top left corner 
+    
+    }
     /**
      * Constructor for objects of class Mirror_Temple
      */
-    public void mirrorTemple()
+    public void mirrorTemple(int [] [] pixels)
     {
         // initialise instance variables
-       
+        int height = pixels.length;
+        int width = pixels[0].length;
+        
     }
 }
