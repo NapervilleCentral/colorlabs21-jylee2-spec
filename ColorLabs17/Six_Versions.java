3cf9979ecb15b73cbdcc7026e089f8d6bef9e5fd
Joshua Lee <jylee2@stu.naperville203.org>
2026-05-06 09:41:37 -0500

    work on sepia and sclaing other stuff

--- ColorLabs17/Six_Versions.java
+++ ColorLabs17/Six_Versions.java
@@ -20,13 +20,130 @@ public class Six_Versions
      
     public static void main (String [] args)
     {
-     Picture Version1 = new Picture ();
-     Picture Version2 = new Picture ();
-     Picture Version3 = new Picture ();
-     Picture Version4 = new Picture ();
-     Picture Version5 = new Picture ();
-     Picture Version6 = new Picture ();
+     // Original
+     Picture Version1 = new Picture ("images/Bob.jpg");
+     
+     // Grayscale
+     Picture Version2 = new Picture ("images/Bob.jpg");
+     
+     // Negation
+     Picture Version3 = new Picture ("images/Bob.jpg");
+     
+     // Reflect on Y-axis
+     Picture Version4 = new Picture ("images/Bob.jpg");
+     
+     // Sepia
+     Picture Version5 = new Picture ("images/Bob.jpg");
+     
+     // Scaling down into top left corner (recursivly done)
+     Picture Version6 = new Picture ("images/Bob.jpg");
+     
+     Version1.explore();
+     
+     method1(Version2);
+     Version2.explore();
+     Version2.write("images/Version2.jpg");
+     
+     method2(Version3);
+     Version3.explore();
+     Version3.write("images/Version3.jpg");
+
+     method3(Version4);
+     //Version4.explore();
+     //Version5.explore();
+     //Version6.explore();
 
     
+    }
+    
+    public static void method1 (Picture Version2)
+    {
+        Pixel[] pixels = Version2.getPixels();
+
+        for (Pixel p : pixels)
+        {
+            int r = p.getRed();
+            int g = p.getGreen();
+            int b = p.getBlue();
+            int gray = (r + g + b) / 3;
+            p.setColor(new Color(gray, gray, gray));
+        }
+
+        
+    }
+    
+    public static void method2 (Picture Version3)
+    {
+        Pixel[] pixels = Version3.getPixels();
+
+        for (Pixel p : pixels)
+        {
+            int r = p.getRed();
+            int g = p.getGreen();
+            int b = p.getBlue();
+            int gray = (r + g + b) / 3;
+            p.setColor(new Color(gray, gray, gray));
+        }
+
+        for (Pixel p : pixels)
+        {
+            int gray = p.getRed();
+
+            Color color1 = new Color(0, 0, 120);
+            Color color2 = new Color(200, 0, 0);
+            Color color3 = new Color(120, 160, 255);
+            Color color4 = new Color(240, 240, 220);
+
+            if (gray < 64)
+                p.setColor(color1);
+            else if (gray < 128)
+                p.setColor(color2);
+            else if (gray < 192)
+                p.setColor(color3);
+            else
+                p.setColor(color4);
+        }
+    }
+    
+    public static void method3 (Picture Version4)
+    {
+        int width = Version4.getWidth();
+        int height = Version4.getHeight();
+    
+        Pixel leftPixel = null;
+        Pixel rightPixel = null; // getting error for pixel at home
+        // possible due to library not being implemented, but cant find the option
+    
+        for (int y = 0; y < height; y++) 
+        {
+            for (int x = 0; x < width / 2; x++) 
+            {
+                leftPixel = Version4.getPixel(x, y);
+                rightPixel = Version4.getPixel(width - 1 - x, y);
+    
+                rightPixel.setColor(leftPixel.getColor());
+            }
+        }
+        Version4.explore();
+    }
+    
+    public static void mehtod4 (Picture Version5)
+    {
+        Pixel[] pixels = Version5.getPixels();
+        
+        for (Pixel p: pixels)
+        {
+            int r = p.getRed();
+            int b = p.getBlue();
+            int g = p.getGreen();
+            
+            int gray = (r + g + b) / 3;
+            p.setColor(new Color(gray, gray, gray));
+        }
+        
+        for (Pixel p: pixels)
+        {
+            
+        }
     }
 }
