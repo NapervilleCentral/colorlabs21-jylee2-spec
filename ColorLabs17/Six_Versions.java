8a3b3ed67b733b7c4bc07fd8e69fa29345d89665
Joshua Lee <jylee2@stu.naperville203.org>
2026-05-14 18:22:30 -0500

    re-did the copy to canvas method, now resize image to be what it was before

--- ColorLabs17/Six_Versions.java
+++ ColorLabs17/Six_Versions.java
@@ -1,6 +1,5 @@
-
 /**
- * Write a description of class Six_Versions here.
+* Write a description of class Six_Versions here.
  *
  * @author (Joshua Lee)
  * @version (5/5/26)
@@ -19,52 +18,58 @@ public class Six_Versions
      *  Sepia
      *  down scaling image
      */
-     
+
     public static void main (String [] args)
     {
-         // Original
-         Picture Version1 = new Picture ("images/Bob.jpg");
-         
-         // Grayscale
-         Picture Version2 = new Picture ("images/Bob.jpg");
-         
-         // Negation
-         Picture Version3 = new Picture ("images/Bob.jpg");
-         
-         // Reflect on Y-axis
-         Picture Version4 = new Picture ("images/Bob.jpg");
-         
-         // Sepia
-         Picture Version5 = new Picture ("images/Bob.jpg");
-         
-         // Scaling down into top left corner (recursivly done)
-         Picture Version6 = new Picture ("images/Bob.jpg");
-         
-         Version1.explore();
-         Version1.write("images/Version1.jpg");
-         
-         method1(Version2);
-         Version2.explore();
-         Version2.write("images/Version2.jpg");
-         
-         method2(Version3);
-         Version3.explore();
-         Version3.write("images/Version3.jpg");
-    
-         method3(Version4);
-         Version4.write("images/Version4.jpg");
-         
-         method4(Version5);
-         Version5.write("images/Version5.jpg");
-         
-         method5(Version6 , 6);
-         Version6.explore();
-         Version6.write("images/Version6.jpg");
-    
-         CopytoCanvas(Version1 , Version1 , 0 , 0);
-         CopytoCanvas(Version1 , Version1 , 300 , 0);
+        // Original
+        Picture canvas = new Picture (1200, 1800);
+        
+        Picture Version1 = new Picture ("images/Bob.jpg");
+
+        // Grayscale
+        Picture Version2 = new Picture ("images/Bob.jpg");
+
+        // Negation
+        Picture Version3 = new Picture ("images/Bob.jpg");
+
+        // Reflect on Y-axis
+        Picture Version4 = new Picture ("images/Bob.jpg");
+
+        // Sepia
+        Picture Version5 = new Picture ("images/Bob.jpg");
+
+        // Scaling down into top left corner (recursivly done)
+        Picture Version6 = new Picture ("images/Bob.jpg");
+
+        Version1.explore();
+        Version1.write("images/Version1.jpg");
+
+        method1(Version2);
+        Version2.explore();
+        Version2.write("images/Version2.jpg");
+
+        method2(Version3);
+        Version3.explore();
+        Version3.write("images/Version3.jpg");
+
+        method3(Version4);
+        Version4.write("images/Version4.jpg");
+
+        method4(Version5);
+        Version5.write("images/Version5.jpg");
+
+        method5(Version6 , 6);
+        Version6.explore();
+        Version6.write("images/Version6.jpg");
+
+        CopytoCanvas(Version1 , canvas , 0 , 0);
+        CopytoCanvas(Version2 , canvas , 399 , 0);
+        CopytoCanvas(Version3 , canvas , 0 , 0);
+        CopytoCanvas(Version4 , canvas , 0 , 0);
+        CopytoCanvas(Version5 , canvas , 0 , 0);
+        CopytoCanvas(Version6 , canvas , 0 , 0);
     }
-    
+
     public static void method1 (Picture Version2)
     {
         Pixel[] pixels = Version2.getPixels();
@@ -78,9 +83,7 @@ public class Six_Versions
             p.setColor(new Color(gray, gray, gray));
         }
 
-        
     }
-    
     public static void method2 (Picture Version3)
     {
         Pixel[] pixels = Version3.getPixels();
@@ -113,107 +116,112 @@ public class Six_Versions
                 p.setColor(color4);
         }
     }
-    
+
     public static void method3 (Picture Version4)
     {
         int width = Version4.getWidth();
         int height = Version4.getHeight();
-    
+
         Pixel leftPixel = null;
         Pixel rightPixel = null;
-        
-    
+
         for (int y = 0; y < height; y++) 
         {
             for (int x = 0; x < width / 2; x++) 
             {
                 leftPixel = Version4.getPixel(x, y);
                 rightPixel = Version4.getPixel(width - 1 - x, y);
-    
+
                 rightPixel.setColor(leftPixel.getColor());
             }
         }
         Version4.explore();
     }
-    
+
     public static void method4 (Picture Version5)
     {
         Pixel[] pixels = Version5.getPixels();
-        
+
         // sets image to gray scale in prep for color change
-        
+
         for (Pixel p: pixels)
         {
             int r = p.getRed();
             int b = p.getBlue();
             int g = p.getGreen();
-            
+
             int gray = (r + g + b) / 3;
             p.setColor(new Color(gray, gray, gray));
         }
-        
+
         // changes color of gray image to sepia, percentage based rgb values
         for (Pixel p: pixels)
         {
             int r = p.getRed();
             int b = p.getBlue();
             int g = p.getGreen();
-            
+
             r = (int) (r * 0.61);
             b = (int) (b * 0.11);
             g = (int) (g * 0.35);
-            
+
             p.setRed(r);
             p.setBlue(b);
             p.setGreen(g);
-            
+
         }
         Version5.explore();
     }
-    
+
     public static void method5(Picture pic, int times)
     {
         // base case
         if (times <= 0)
             return;
-    
+
         int width = pic.getWidth();
         int height = pic.getHeight();
-    
+
         // create a copy to read from
         Picture copy = new Picture("images/Bob.jpg");
-    
+
         // scale factor (each time half size)
         double scale = Math.pow(0.5, 6 - times);
-    
+
         for (int y = 0; y < height * scale; y++)
         {
             for (int x = 0; x < width * scale; x++)
             {
                 Pixel target = pic.getPixel(x, y);
-    
+
                 Pixel source = copy.getPixel((int)(x / scale), (int)(y / scale));
-    
+
                 target.setColor(source.getColor());
             }
         }
-    
+
         // recursive call
         method5(pic, times - 1);
     }
-    
-    public static void CopytoCanvas( Picture pic , Picture target, int Xpos , int Ypos)
+
+    public static void CopytoCanvas(Picture pic, Picture target, int Xpos, int Ypos)
     {
         Pixel sourcePix = null;
         Pixel targetPix = null;
-        
-       
-        targetPix = target.getPixel(Xpos , Ypos);
-        sourcePix = pic.getPixel(Xpos , Ypos);
-        targetPix.setColor(sourcePix.getColor());
-        
-        
-            
-        target.explore();
+    
+        int width = pic.getWidth();
+        int height = pic.getHeight();
+    
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                sourcePix = pic.getPixel(x, y);
+    
+                targetPix = target.getPixel(x + Xpos, y + Ypos);
+    
+                targetPix.setColor(sourcePix.getColor());
+            }
+        }
     }
 }
